7923f7bf1835f1432a3c757942a5d8f7441931d7
bjmwj <bj.mwj@hotmail.com>
2016-02-09 23:37:02 +1100

    2

--- wsgi/mybottleapp.py
+++ wsgi/mybottleapp.py
@@ -4,42 +4,42 @@ from bottle import route, default_app, static_file
 @route('/')
 @route('/index')
 def index():
-    return static_file('index.html', root='static/static')
+    return static_file('index.html', root='static')
 
 
 @route('/jglxt')
 def jglxt():
-    return static_file('jglxt.html', root='static/static')
+    return static_file('jglxt.html', root='static')
 
 
 @route('/wxgzcs')
 def wxgzcs():
-    return static_file('wxgzcs.html', root='static/static')
+    return static_file('wxgzcs.html', root='static')
 
 
 @route('/zwjg')
 def zwjg():
-    return static_file('zwjg.html', root='static/static')
+    return static_file('zwjg.html', root='static')
 
 
 @route('/contact')
 def contact():
-    return static_file('contact.html', root='static/static')
+    return static_file('contact.html', root='static')
 
 
 @route('/css/:path#.+#', name='css')
 def static(path):
-    return static_file(path, root='static/static/css')
+    return static_file(path, root='static/css')
 
 
 @route('/images/:path#.+#', name='images')
 def static(path):
-    return static_file(path, root='static/static/images')
+    return static_file(path, root='static/images')
 
 
 @route('/js/:path#.+#', name='js')
 def static(path):
-    return static_file(path, root='static/static/js')
+    return static_file(path, root='static/js')
 
 
 # This must be added in order to do correct path lookups for the views
