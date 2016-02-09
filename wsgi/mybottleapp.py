3fac7a8d653b1018afa306a40d1c4bc19b4043b2
bjmwj <bj.mwj@hotmail.com>
2016-02-09 22:22:44 +1100

    init

--- wsgi/mybottleapp.py
+++ wsgi/mybottleapp.py
@@ -1,16 +1,50 @@
-from bottle import route, default_app
+from bottle import route, default_app, static_file
 
-@route('/name/<name>')
-def nameindex(name='Stranger'):
-    return '<strong>Hello, %s!</strong>' % name
- 
+
 @route('/')
+@route('/index')
 def index():
-    return '<strong>Hello World!</strong>'
+    return static_file('index.html', root='static/index.html')
+
+
+@route('/jglxt')
+def jglxt():
+    return static_file('jglxt.html', root='static/jglxt.html')
+
+
+@route('/wxgzcs')
+def wxgzcs():
+    return static_file('wxgzcs.html', root='static/wxgzcs.html')
+
+
+@route('/zwjg')
+def zwjg():
+    return static_file('zwjg.html', root='static/zwjg.html')
+
+
+@route('/contact')
+def contact():
+    return static_file('contact.html', root='static/contact.html')
+
+
+@route('/css/:path#.+#', name='css')
+def static(path):
+    return static_file(path, root='static/css')
+
+
+@route('/images/:path#.+#', name='images')
+def static(path):
+    return static_file(path, root='static/images')
+
+
+@route('/js/:path#.+#', name='js')
+def static(path):
+    return static_file(path, root='static/js')
+
 
 # This must be added in order to do correct path lookups for the views
 import os
 from bottle import TEMPLATE_PATH
-TEMPLATE_PATH.append(os.path.join(os.environ['OPENSHIFT_REPO_DIR'], 'wsgi/views/')) 
+TEMPLATE_PATH.append(os.path.join(os.environ['OPENSHIFT_REPO_DIR'], 'wsgi/static/'))
 
 application=default_app()
